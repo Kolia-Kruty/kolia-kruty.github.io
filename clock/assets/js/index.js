aa38fe501b119e3c9f78998d60d117b24beac1ad
Микола <koliabaranovsky16@gmail.com>
2021-01-31 20:32:03 +0200

    script jquery

--- clock/assets/js/index.js
+++ clock/assets/js/index.js
@@ -1,7 +1,7 @@
-// $(".icon-menu-open").click(function(){
-//     $(".nav").show()
-// })
+$(".icon-menu-open").click(function(){
+    $(".nav").show()
+})
 
-// $(".icon-menu-close").click(function(){
-//     $(".nav").hide()
-// })
+$(".icon-menu-close").click(function(){
+    $(".nav").hide()
+})
