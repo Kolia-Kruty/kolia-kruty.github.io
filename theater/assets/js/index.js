7bed1a3d90aa7f912aa68060a3a2619e6a24a2a8
Микола <koliabaranovsky16@gmail.com>
2021-02-08 09:35:14 +0200

    update-1

--- theater/assets/js/index.js
+++ theater/assets/js/index.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
     $(".owl-carousel.projects_slider").owlCarousel({
         items: 4,
         loop: true,
-        freeDrag: false,
+        // freeDrag: false,
         autoWidth: true,
     });
 });
@@ -44,18 +44,5 @@ $(".carousel_nav_next").click(function () {
     owl.trigger('next.owl.carousel');
 })
 
-
-var $projectsSliderItem
-
-$('.projects_slider_item ').click(function () {
-    if ($projectsSliderItem) {
-        $projectsSliderItem.find(".front_img").show()
-        $projectsSliderItem.find(".front_img_search").hide()
-    }
-    $projectsSliderItem = $(this)
-    $projectsSliderItem.find(".front_img").hide()
-    $projectsSliderItem.find(".front_img_search").show()
-
-})
 
 
